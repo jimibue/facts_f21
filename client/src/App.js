fc19eb598133c89d46ca8de10d544bb6265084e9
James Yeates <jimibue@gmail.com>
2021-09-03 11:16:27 -0600

    react setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Facts from "./components/Facts";
+import NavBar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <p>navbar</p>
-      <Facts />
-      <p>footer</p>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/facts" component={Facts} />
+        <Route component={() => <h1>Not found</h1>} />
+      </Switch>
     </div>
   );
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # all api endpoint (routes) go in here
+  namespace :api do
+    get '/facts', to:'facts#index'
+  end
 end
